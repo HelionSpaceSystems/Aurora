ab9ce94c1ab5b22eb72376c4af11ae38feea9859
HSS <helionspace@gmail.com>
2016-03-28 12:09:49 -0400

    re-structured functions into lv module and renamed untitled0.py to rocketConcept.py

--- aerodynamicfunc/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 16 19:42:46 2015
-
-@author: alib
-"""
-
-from aerodynamicfunc import aerodynamicfunc
--- launch_vehicle/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar  5 07:47:47 2016
-
-@author: alib
-"""
-from structure import mass_estimates
-from propulsion import propulsion
-
